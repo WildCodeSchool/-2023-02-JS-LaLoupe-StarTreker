3183248311ece7e2a963b6ca96d43b8fcb70a766
Quentin Silmain <quentin1813@yahoo.fr>
2023-04-18 14:50:43 +0200

    The style of the header, the burgermenu and the footer is now updated.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import style from "./componentsCss/Footer.module.scss";
+import PanelImg from "../assets/Panel_BG.jpg";
 
 function Footer() {
   return (
     <footer className={style.footer}>
-      <p className={style.footer}>© Star Treker 2023</p>
+      <div className={style.panel1} />
+      <p className={style.footer}>©℗®™ Star Treker 2023</p>
       <button type="button" className={style.footer}>
         support
       </button>
+      <img className={style.panelimg} src={PanelImg} alt="Stars Background" />
     </footer>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,20 @@
 import style from "@components/componentsCss/Header.module.scss";
 import BurgerMenu from "@components/BurgerMenu";
+import PanelImg from "../assets/Panel_BG.jpg";
 import Logo from "../assets/ST_Logo.png";
 
 function Header() {
   return (
     <header className={style.header}>
-      <img className={style.header} src={Logo} alt="Logo" />
-      <h1 className={style.header}>Star Treker</h1>
+      <div className={style.panel1} />
+      <div className={style.panel2} />
+      <img className={style.logo} src={Logo} alt="Logo" />
+      <h1 className={style.title}>
+        Star <br />
+        ‏‏‎ ‎ Treker
+      </h1>
+      <img className={style.panelimg} src={PanelImg} alt="Stars Background" />
       <BurgerMenu />
-      <section className={style.header}>
-        <section>
-          <div className={style.header} />
-          <div className={style.header} />
-        </section>
-        <section>
-          <div className={style.header} />
-          <div className={style.header} />
-        </section>
-      </section>
     </header>
   );
 }
