d831cd5f301b9481755b55cf43acae0e39678f48
Benjamin Laroche <benjamin.laroche28@gmail.com>
2023-04-19 11:31:29 +0200

    merge incoming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import "./App.scss";
+import Header from "@components/Header";
 import Footer from "@components/Footer";
+import "./components/componentsCss/Background.scss";
+import bgimg from "./assets/Map-Milky-Way.png";
+import SearchBar from "./components/Search";
 
 function App() {
   return (
     <div>
+      <Header />
+      <main>
+        <img src={bgimg} alt="Milky Way" />
+        <SearchBar />
+      </main>
+
       <Footer />
     </div>
   );
