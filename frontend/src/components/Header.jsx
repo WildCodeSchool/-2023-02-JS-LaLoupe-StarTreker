9a34ade17b1481e5c55dc6c04973ab565ca8ef48
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-04-20 16:38:03 +0200

    update scroll et link star trek

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import style from "@components/componentsCss/Header.module.scss";
 import BurgerMenu from "@components/BurgerMenu";
 import PanelImg from "../assets/Panel_BG.jpg";
 import Logo from "../assets/ST_Logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -9,10 +10,12 @@ function Header() {
       <div className={style.panel1} />
       <div className={style.panel2} />
       <img className={style.logo} src={Logo} alt="Logo" />
-      <h1 className={style.title}>
-        Star <br />
-        ‏‏‎ ‎ Treker
-      </h1>
+      <Link className={style.blop} to="/">
+        <h1 className={style.title}>
+          Star <br />
+          ‏‏‎ ‎ Treker
+        </h1>
+      </Link>
       <img className={style.panelimg} src={PanelImg} alt="Stars Background" />
       <BurgerMenu />
     </header>
