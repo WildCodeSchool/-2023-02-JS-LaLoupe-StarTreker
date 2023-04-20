842c8eb168d11cb61f39af8144a71d7966d20d91
alicia coquerant <alicia.coquerant@gmail.com>
2023-04-20 14:55:58 +0200

    fixed searchbar display

--- frontend/src/pages/Gps.jsx
+++ frontend/src/pages/Gps.jsx
@@ -1,9 +1,11 @@
 import bgimg from "../assets/Map-Milky-Way.png";
+import Search from "../components/Search";
 
 function Gps() {
   return (
     <main>
       <img src={bgimg} alt="Milky Way" />
+      <Search />
     </main>
   );
 }
