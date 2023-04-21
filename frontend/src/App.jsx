1cf7fa0cfa7b486a5092e317907b0ea1691ede02
alicia coquerant <alicia.coquerant@gmail.com>
2023-04-21 11:44:22 +0200

    update planetPage and planetCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,19 @@ function App() {
   return (
     <div>
       <Router>
-        <Header />
-        <div>
-          <Routes>
-            <Route path="/" element={<Gps />} />
-            <Route path="/actu" element={<Header />} />
-            <Route path="/planetes" element={<Planets />} />
-            <Route path="/support" element={<Support />} />
-          </Routes>
+        <div className="general">
+          <Header />
+          <div>
+            <Routes>
+              <Route path="/" element={<Gps />} />
+              <Route path="/actu" element={<Header />} />
+              <Route path="/planetes" element={<Planets />} />
+              <Route path="/support" element={<Support />} />
+            </Routes>
+            <Footer />
+          </div>
         </div>
       </Router>
-      <Footer />
     </div>
   );
 }
