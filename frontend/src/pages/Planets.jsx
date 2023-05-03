a99c3f1a5fe667104eb4cd36204832b7f857613b
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-05-03 14:48:50 +0200

    mise en place d'un chargement + enlever la decription des cartes planetes + centrer les textes du formulaire + z index de la barre de recherche + fixer le footer

--- frontend/src/pages/Planets.jsx
+++ frontend/src/pages/Planets.jsx
@@ -1,22 +1,14 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Loading from "@components/Loading";
 import PlanetCard from "../components/PlanetCard";
 import bgimgplanet from "../assets/planetNebula.png";
 import style from "../components/componentsCss/planetsPage.module.scss";
 
-const samplePlanet = {
-  kepoi_name: "K00868.01",
-  koi_period: 235.9986399,
-  koi_prad: 10.59,
-  koi_teq: 188,
-  koi_steff: 4245,
-  koi_srad: 0.657,
-  koi_disposition: "CANDIDATE",
-  koi_score: 0.998,
-};
-
 const getPlanet = () => {
-  const [planets, setPlanets] = useState([samplePlanet]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [planets, setPlanets] = useState();
+
   useEffect(() => {
     axios
       .get(
@@ -24,22 +16,27 @@ const getPlanet = () => {
       )
       .then((response) => {
         setPlanets(response.data.slice(0, 70));
+        setIsLoading(false);
       });
   }, []);
 
-  return (
-    <>
-      <img src={bgimgplanet} alt="Nebula with planet" className={style.bg} />
-      <div className={style.planetsPage}>
-        <h2>Planètes</h2>
-        <section>
-          {planets.map((planet) => (
-            <PlanetCard key={`planet-${planet.kepid}`} planet={planet} />
-          ))}
-        </section>
-      </div>
-    </>
-  );
+  if (isLoading) {
+    <Loading />;
+  } else {
+    return (
+      <>
+        <img src={bgimgplanet} alt="Nebula with planet" className={style.bg} />
+        <div className={style.planetsPage}>
+          <h2>Planètes</h2>
+          <section>
+            {planets.map((planet) => (
+              <PlanetCard key={`planet-${planet.kepid}`} planet={planet} />
+            ))}
+          </section>
+        </div>
+      </>
+    );
+  }
 };
 
 export default getPlanet;
